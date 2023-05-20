b85f3b965513ccac4477d2e7055e1ec3b6e0ab14
VODINHQUOC <quoc20121998@gmail.com>
2023-05-20 22:47:08 +0700

    Add Ecommerce Entity and Repository

---
+++ src/main/java/com/k2pan/api/models/ecommerce/Order.java
@@ -0,0 +1,37 @@
+package com.k2pan.api.models.ecommerce;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @Column(name = "order_date")
+    private LocalDate orderDate;
+
+    @Column(name = "total_amount")
+    private BigDecimal totalAmount;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @OneToMany(mappedBy = "orders")
+    private List<OrderItem> orderItems;
+}
